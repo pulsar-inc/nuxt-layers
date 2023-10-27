551b6ed75ce9c7196655f570a01f6ee05218881a
Arnaud Gissinger <agissing@student.42.fr>
2023-10-27 17:51:21 +0200

    [UPDATE] playground

---
+++ playground/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <Html class="h-screen w-screen overflow-hidden">
+    <Body class="h-screen w-screen overflow-hidden bg-slate-900">
+      <main
+        class="flex flex-col gap-5 justify-center items-center h-screen w-screen animate-ping"
+      >
+        <Icon
+          name="heroicons:trophy-20-solid"
+          class="text-yellow-400 h-24 w-24"
+        />
+        <p class="text-3xl font-medium text-yellow-500">Well done!</p>
+      </main>
+    </Body>
+  </Html>
+</template>

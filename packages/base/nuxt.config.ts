7e6c493b802a9d777a7df847d9767060ec597722
Arnaud Gissinger <agissing@student.42.fr>
2023-11-22 14:33:23 +0100

    fix: missing nuxt image declaration in modules

--- packages/base/nuxt.config.ts
+++ packages/base/nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/eslint-module", "@nuxtjs/tailwindcss", "nuxt-icon"],
+  modules: [
+    "@nuxtjs/eslint-module",
+    "@nuxtjs/tailwindcss",
+    "@nuxt/image",
+    "nuxt-icon",
+  ],
   devtools: { enabled: true },
   postcss: {
     plugins: {
